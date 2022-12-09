2d4fbaca72745ec7a55afb6adf0784c52f0ade78
Jukka Raimovaara <roska@mentalhouse.fi>
2022-12-09 17:17:07 +0200

    delay to cronjob script

--- renew.sh
+++ renew.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd $(dirname "${BASH_SOURCE[0]}")
+
+# Sleep for a random amount of time because we're nice to letsencrypt
+sleep $[($RANDOM % 120)+1]
+
 certbot renew \
   --config-dir ./conf --work-dir ./tmp --logs-dir ./logs
